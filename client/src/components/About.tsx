14fc8774f7306418f243f59840b5809eaa624207
Tim Silva <tim17silva@gmail.com>
2023-08-22 09:08:30 -0700

    Started work on about section

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,4 +1,13 @@
 export default function About() {
-  return;
-  <div id="about"></div>;
+  return (
+    <section id="about" className="bg-[#1A1D20] h-[66vh]">
+      <h1 className="p-12 text-4xl text-center">About</h1>
+      <div className="flex">
+        <div className="text-center basis-2/5">
+          <h2>Tim Silva</h2>
+        </div>
+        <div className="text-left basis-3/5">dwia</div>
+      </div>
+    </section>
+  );
 }
