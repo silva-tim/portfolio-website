6aa5cf8a8adf92861cd9e356522e4547bdd8afe3
Tim Silva <tim17silva@gmail.com>
2023-08-22 15:45:58 -0700

    Worked on about page, successfully deployed

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,6 +1,9 @@
 export default function About() {
   return (
-    <section id="about" className="bg-[#1A1D20] min-h-screen sm:min-h-[66vh]">
+    <section
+      id="about"
+      className="bg-[#1A1D20] scroll-m-10 sm:scroll-m-20 min-h-screen sm:min-h-[66vh]"
+    >
       <h1 className="p-12 font-mono text-4xl text-center">About</h1>
       <div className="flex flex-wrap w-11/12 m-auto">
         <div className="flex flex-wrap justify-center h-60 basis-full lg:basis-1/3">
@@ -8,7 +11,7 @@ export default function About() {
             <img
               src="/me.png"
               alt=""
-              className="m-auto rounded-full bg-mainLight h-1/2"
+              className="m-auto rounded-full bg-slate-500 h-1/2"
             />
             <h2 className="mt-6 text-3xl lg:mt-10">Tim Silva</h2>
           </div>
@@ -27,14 +30,18 @@ export default function About() {
             projects to learn the technologies that are used in the industry
             today. My current interests revolve around front-end web
             development, mostly{" "}
-            <a href="https://react.dev/" target="_blank" className="underline">
+            <a
+              href="https://react.dev/"
+              target="_blank"
+              className="underline text-secondary"
+            >
               React
             </a>{" "}
             and{" "}
             <a
               href="https://tailwindcss.com/"
               target="_blank"
-              className="underline"
+              className="underline text-secondary"
             >
               Tailwind
             </a>{" "}
