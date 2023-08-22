888da0daf4c6f092c150323ccfd8bcb12eb664b7
Tim Silva <tim17silva@gmail.com>
2023-08-22 12:01:48 -0700

    Finished about section

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -4,15 +4,15 @@ export default function About() {
   return (
     <section id="about" className="bg-[#1A1D20] min-h-screen sm:min-h-[66vh]">
       <h1 className="p-12 text-4xl text-center">About</h1>
-      <div className="flex flex-wrap">
-        <div className="flex flex-wrap justify-center basis-full lg:basis-1/3">
-          <div className="flex justify-center basis-full">
-            <div className="overflow-hidden bg-gray-400 rounded-full basis-1/3 lg:basis-2/5">
-              <img src="../../public/me.png" alt="" />
-            </div>
-          </div>
-          <div className="pt-8">
-            <h2 className="text-3xl">Tim Silva</h2>
+      <div className="flex flex-wrap w-11/12 m-auto">
+        <div className="flex flex-wrap justify-center h-60 basis-full lg:basis-1/3">
+          <div className="text-center">
+            <img
+              src="../../public/me.png"
+              alt=""
+              className="m-auto bg-gray-400 rounded-full h-1/2"
+            />
+            <h2 className="mt-6 text-3xl lg:mt-10">Tim Silva</h2>
           </div>
         </div>
         <div className="p-4 text-center lg:text-left lg:text-xl basis-full lg:basis-2/3">
