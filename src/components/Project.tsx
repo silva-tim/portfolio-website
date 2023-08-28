3d316a21ba8b9890a5f4d2eda639bc3d181760b1
Tim Silva <tim17silva@gmail.com>
2023-08-28 15:42:59 -0700

    Started mobile friendliness

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,12 +11,12 @@ type props = {
 
 export default function Project({ project }: props) {
   return (
-    <div className="relative flex w-1/2 overflow-hidden text-black transition bg-white rounded-sm h-80 group hover:bg-opacity-90">
-      <div className="flex flex-wrap pt-10 basis-1/2 ps-10">
-        <h2 className="text-2xl basis-full">{project.name}</h2>
-        <p className="h-32 py-2">{project.desc}</p>
+    <div className="relative flex flex-wrap mx-4 overflow-hidden text-black transition bg-white rounded-sm md:w-1/2 md:h-80 group hover:bg-opacity-90">
+      <div className="flex flex-wrap justify-center pt-4 md:pt-10 basis-full md:basis-1/2 md:ps-10">
+        <h2 className="text-2xl text-center basis-full">{project.name}</h2>
+        <p className="py-2 text-center md:h-32">{project.desc}</p>
         <div className="basis-full">
-          <ul className="flex flex-wrap gap-1">
+          <ul className="flex flex-wrap justify-center gap-1">
             {project.skills.map((i) => (
               <li key={i} className="px-4 text-sm rounded-full bg-secondary">
                 {i}
@@ -41,7 +41,7 @@ export default function Project({ project }: props) {
           </a>
         </div>
       </div>
-      <div className="absolute w-3/4 transition top-14 group-hover:-translate-x-2 group-hover:translate-y-2 group-hover:-rotate-2 -right-80">
+      <div className="w-full px-10 py-5 transition md:w-3/4 md:absolute top-14 group-hover:-translate-x-2 group-hover:translate-y-2 group-hover:-rotate-2 -right-80">
         <img src={project.image} alt="" className="scale-125 rounded-md" />
       </div>
     </div>
