ec12be97064ddaa990826b8f4764f2dc3e88f7a7
Tim Silva <tim17silva@gmail.com>
2023-09-06 11:54:43 -0700

    Fixed bounciness

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,13 +4,17 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="pb-10 scroll-m-10 sm:scroll-m-18">
+    <section
+      id="projects"
+      className="pb-10 scroll-m-10 sm:scroll-m-18 bg-[#1A1D20]"
+    >
       <h1 className="p-12 font-mono text-4xl text-center">Projects</h1>
       <div className="flex flex-wrap justify-center gap-4">
         {projects.map((i) => (
           <motion.div
             initial={{ scale: 0, opacity: 0 }}
             whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ bounce: 0 }}
             className="flex justify-center w-full"
             key={i.projectNum}
           >
--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -10,7 +10,7 @@ export default function SkillCard({ skill }: props) {
   return (
     <a
       href={skill.link}
-      className="xl:basis-1/6 basis-full sm:basis-1/3"
+      className="transition xl:basis-1/6 basis-full sm:basis-1/3 hover:scale-105"
       target="_blank"
     >
       <div className="flex flex-wrap justify-center h-40 bg-white rounded-sm">
