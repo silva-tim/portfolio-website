1d01cc6b9e37022921b2ce9fea2e8dacada3c203
Tim Silva <tim17silva@gmail.com>
2023-09-08 14:00:36 -0700

    Color change

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,15 +6,15 @@ export default function Contact() {
     <section id="contact" className="pb-10">
       <h1 className="p-12 font-mono text-4xl text-center">Contact</h1>
       <div className="flex justify-center gap-8">
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl underline rounded-sm bg-secondary text-main">
+        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight text-secondary">
           <FiMail />
           <a href="mailto: tim@silvatim.com">tim@silvatim.com</a>
         </div>
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl underline rounded-sm text-main bg-secondary">
+        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline text-secondary bg-mainLight">
           <FaLinkedinIn />
           <a href="https://www.linkedin.com/in/silva-tim/">silva-tim</a>
         </div>
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl underline rounded-sm text-main bg-secondary">
+        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline text-secondary bg-mainLight">
           <FaGithub />
           <a href="https://github.com/silva-tim">silva-tim</a>
         </div>
