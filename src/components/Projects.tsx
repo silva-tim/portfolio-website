8b5b423f04f8c8050f9af01b54704aa115ee1d1e
Tim Silva <tim17silva@gmail.com>
2023-09-12 13:50:13 -0700

    Changed background and added mobile design for contact

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,10 +4,7 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section
-      id="projects"
-      className="pb-20 scroll-m-10 sm:scroll-m-18 bg-[#1A1D20]"
-    >
+    <section id="projects" className="pb-20 scroll-m-10 sm:scroll-m-18 bg-main">
       <h1 className="p-12 font-mono text-4xl text-center">Projects</h1>
       <div className="flex flex-wrap justify-center gap-4">
         {projects.map((i) => (
