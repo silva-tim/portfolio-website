cb8620e9decdc727ed519081075afaf00d389de3
Tim Silva <tim17silva@gmail.com>
2023-10-02 19:28:02 -0700

    Adding motion to other sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import { useInView } from "react-intersection-observer";
 import { useActiveContext } from "../context/activeContext";
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 export default function About() {
@@ -20,51 +21,56 @@ export default function About() {
       id="about"
       className="bg-[#1A1D20] scroll-m-10 sm:scroll-m-24 min-h-screen sm:min-h-[66vh]"
     >
-      <h1 className="p-12 font-mono text-4xl text-center">About</h1>
-      <div className="flex flex-wrap w-11/12 m-auto">
-        <div className="flex flex-wrap justify-center h-60 basis-full lg:basis-1/3">
-          <div className="text-center">
-            <img
-              src="/images/me.png"
-              alt=""
-              className="m-auto rounded-full bg-slate-500 h-1/2"
-            />
-            <h2 className="mt-6 text-3xl lg:mt-10">Tim Silva</h2>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <h1 className="p-12 font-mono text-4xl text-center">About</h1>
+        <div className="flex flex-wrap w-11/12 m-auto">
+          <div className="flex flex-wrap justify-center h-60 basis-full lg:basis-1/3">
+            <div className="text-center">
+              <img
+                src="/images/me.png"
+                alt=""
+                className="m-auto rounded-full bg-slate-500 h-1/2"
+              />
+              <h2 className="mt-6 text-3xl lg:mt-10">Tim Silva</h2>
+            </div>
+          </div>
+          <div className="p-4 text-center lg:text-left lg:text-xl basis-full lg:basis-2/3">
+            <p>
+              I have always had a strong passion for computers/technology and
+              after spending a few years working at a financial advisory firm I
+              realized that I wanted a career that pursued these passions more.
+              I decided to change paths and began to explore the world of
+              programming.
+            </p>
+            <br />
+            <p>
+              I enrolled in an accelerated program where I created several
+              projects to learn the technologies that are used in the industry
+              today. My current interests revolve around front-end web
+              development, mostly{" "}
+              <a
+                href="https://react.dev/"
+                target="_blank"
+                className="underline text-secondary"
+              >
+                React
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                className="underline text-secondary"
+              >
+                Tailwind
+              </a>{" "}
+              but I am always seeking to learn more.
+            </p>
           </div>
         </div>
-        <div className="p-4 text-center lg:text-left lg:text-xl basis-full lg:basis-2/3">
-          <p>
-            I have always had a strong passion for computers/technology and
-            after spending a few years working at a financial advisory firm I
-            realized that I wanted a career that pursued these passions more. I
-            decided to change paths and began to explore the world of
-            programming.
-          </p>
-          <br />
-          <p>
-            I enrolled in an accelerated program where I created several
-            projects to learn the technologies that are used in the industry
-            today. My current interests revolve around front-end web
-            development, mostly{" "}
-            <a
-              href="https://react.dev/"
-              target="_blank"
-              className="underline text-secondary"
-            >
-              React
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://tailwindcss.com/"
-              target="_blank"
-              className="underline text-secondary"
-            >
-              Tailwind
-            </a>{" "}
-            but I am always seeking to learn more.
-          </p>
-        </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ import { FiMail } from "react-icons/fi";
 import { useInView } from "react-intersection-observer";
 import { useActiveContext } from "../context/activeContext";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function Contact() {
   const { ref, inView } = useInView({
@@ -18,35 +19,40 @@ export default function Contact() {
 
   return (
     <section ref={ref} id="contact" className="pb-10">
-      <h1 className="p-12 font-mono text-4xl text-center">Contact</h1>
-      <div className="flex flex-wrap justify-center gap-8">
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
-          <FiMail />
-          <a href="mailto: tim@silvatim.com" className="text-secondary">
-            tim@silvatim.com
-          </a>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <h1 className="p-12 font-mono text-4xl text-center">Contact</h1>
+        <div className="flex flex-wrap justify-center gap-8">
+          <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
+            <FiMail />
+            <a href="mailto: tim@silvatim.com" className="text-secondary">
+              tim@silvatim.com
+            </a>
+          </div>
+          <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
+            <FaLinkedinIn />
+            <a
+              href="https://www.linkedin.com/in/silva-tim/"
+              target="_blank"
+              className="text-secondary"
+            >
+              silva-tim
+            </a>
+          </div>
+          <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
+            <FaGithub />
+            <a
+              href="https://github.com/silva-tim"
+              target="_blank"
+              className="text-secondary"
+            >
+              silva-tim
+            </a>
+          </div>
         </div>
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
-          <FaLinkedinIn />
-          <a
-            href="https://www.linkedin.com/in/silva-tim/"
-            target="_blank"
-            className="text-secondary"
-          >
-            silva-tim
-          </a>
-        </div>
-        <div className="flex items-center gap-3 px-3 py-2 text-2xl rounded-sm hover:underline bg-mainLight">
-          <FaGithub />
-          <a
-            href="https://github.com/silva-tim"
-            target="_blank"
-            className="text-secondary"
-          >
-            silva-tim
-          </a>
-        </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
