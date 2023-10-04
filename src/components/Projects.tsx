3fcab5920763c49e226ea2e5d47c08842471657a
Tim Silva <tim17silva@gmail.com>
2023-10-03 19:47:41 -0700

    Added more motion

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import Project from "./ProjectCard";
 import { projects } from "../data";
 import { useInView } from "react-intersection-observer";
 import { useActiveContext } from "../context/activeContext";
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 export default function Projects() {
@@ -22,14 +23,19 @@ export default function Projects() {
       id="projects"
       className="pb-20 scroll-m-10 sm:scroll-m-18 bg-main"
     >
-      <h1 className="p-12 font-mono text-4xl text-center">Projects</h1>
-      <div className="flex flex-wrap justify-center gap-4">
-        {projects.map((i) => (
-          <>
-            <Project project={i} />
-          </>
-        ))}
-      </div>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <h1 className="p-12 font-mono text-4xl text-center">Projects</h1>
+        <div className="flex flex-wrap justify-center gap-4">
+          {projects.map((i) => (
+            <>
+              <Project project={i} />
+            </>
+          ))}
+        </div>
+      </motion.div>
     </section>
   );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,19 +19,24 @@ export default function Skills() {
 
   return (
     <section ref={ref} id="skills" className="pb-20 scroll-m-10 sm:scroll-m-18">
-      <h1 className="p-12 font-mono text-4xl text-center">My Skills</h1>
-      <div className="flex flex-wrap justify-center w-3/5 gap-4 m-auto">
-        {skills.map((i) => (
-          <motion.div
-            initial={{ scale: 0, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1 }}
-            className="xl:basis-1/6 basis-full sm:basis-1/3 hover:scale-105"
-            key={i.name}
-          >
-            <SkillCard skill={i} />
-          </motion.div>
-        ))}
-      </div>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <h1 className="p-12 font-mono text-4xl text-center">My Skills</h1>
+        <div className="flex flex-wrap justify-center w-3/5 gap-4 m-auto">
+          {skills.map((i) => (
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              className="xl:basis-1/6 basis-full sm:basis-1/3 hover:scale-105"
+              key={i.name}
+            >
+              <SkillCard skill={i} />
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
     </section>
   );
 }
